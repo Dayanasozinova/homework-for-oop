481dbb69fca33767b09d64044df365921d4c5701
Dayana Sozinova <daanasozinova6@gmail.com>
2021-09-28 21:03:46 +0300

    Задача 4 исправлена

--- ex 3.py
+++ ex 3.py
@@ -9,7 +9,7 @@ class Student:
     self.courses_attached = {}
 
   def grade_for_a_lectur(self, lecturer, course, grade):
-    if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+    if isinstance(lecturer, Lecturer) and  course in lecturer.courses_attached:
       if course in lecturer.grades:
         lecturer.grades[course] += [grade]
       else:
@@ -49,7 +49,7 @@ class Mentor:
 class Lecturer(Mentor):
   def __init__(self,name, surname):
     super().__init__(name, surname)
-    self.courses_attached = []
+    self.courses_attached = ['Python']
     self.grades = {}
 
   def medium_grades(self):    
@@ -98,7 +98,7 @@ best_student.courses_in_progress += ['Python']
 best_student.finished_courses += ['Введение в программирование']
 
 second_student = Student('Bob', 'Grenn', 'girl')
-second_student.courses_in_progress += ['Geology']
+second_student.courses_in_progress += ['Python']
 
 
 nice_reviewer = Reviewer('Dayana', 'Sozinova')
@@ -106,14 +106,17 @@ cool_reviewer = Reviewer('Some', 'Buddy')
 cool_reviewer.courses_attached += ['Python']
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(second_student, 'Geology', 8)
+cool_reviewer.rate_hw(second_student, 'Python', 8)
 # print(best_student.grades)
 
 interesting_lecturer = Lecturer('Artur', 'Red')
 cool_lecturer = Lecturer('Sam', 'Smith')
-cool_lecturer.courses_attached += ['Geology']
-second_student.grade_for_a_lectur(cool_lecturer, 'Geology', 9)
-# print(cool_lecturer.grades)
+# cool_lecturer.courses_attached += ['Python']
+second_student.grade_for_a_lectur(cool_lecturer, 'Python', 9)
+second_student.grade_for_a_lectur(cool_lecturer, 'Python', 10)
+second_student.grade_for_a_lectur(cool_lecturer, 'Python', 7)
+second_student.grade_for_a_lectur(interesting_lecturer, 'Python', 7)
+print(cool_lecturer.grades)
 
 
 print(nice_reviewer)
@@ -124,3 +127,39 @@ print(best_student)
 print('\n')
 print(best_student < second_student)
 print(interesting_lecturer < cool_lecturer)
+
+def avg_grade_students_by_course(students, course):
+  all_grades = []  
+  for student in students:
+    if course in student.courses_in_progress:
+      all_grades += student.grades[course]      
+    sum = 0
+    quantity = 0
+    for grade in all_grades:
+          sum += grade
+          quantity += 1
+          average = sum / quantity
+  return average
+
+def avg_grade_lectures_by_course(lectures, course):
+  all_grades = []  
+  for lecture in lectures:
+    if course in lecture.courses_attached:
+      all_grades += lecture.grades[course]      
+    sum = 0
+    quantity = 0
+    for grade in all_grades:
+          sum += grade
+          quantity += 1
+          average = sum / quantity
+  return average
+
+
+
+
+
+avg_students = avg_grade_students_by_course([best_student, second_student],'Python')    
+print(avg_students)
+
+avg_lectures = avg_grade_lectures_by_course([cool_lecturer, interesting_lecturer],'Python') 
+print(avg_lectures)
